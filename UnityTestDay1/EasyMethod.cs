79659c005f811e78c6b9d83a9f2ee0ee0a82f0b6
gn02618594 <gn02618594@gmail.com>
2020-10-06 22:22:14 +0800

    feat: 簡易的單元測試

---
+++ UnityTestDay1/EasyMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnityTestDay1
+{
+    public class EasyMethod
+    {
+        private int BaseNumber = 2;
+        public int Method1()
+        {
+            return BaseNumber + 5;
+        }
+        public int Method2()
+        {
+            return BaseNumber + 8;
+        }
+    }
+}
---
+++ UnityTestDay1Tests/EasyMethodTests.cs
@@ -0,0 +1,36 @@
+﻿using UnityTestDay1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnityTestDay1.Tests
+{
+    [TestClass()]
+    public class EasyMethodTests
+    {
+        [TestMethod()]
+        public void Method1Test_乎較時應回傳結果維7()
+        {
+            var Sut = new EasyMethod();
+            var expected = 7;
+
+            var actual = Sut.Method1();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Method2Test_乎較時應回傳結果維10()
+        {
+            var Sut = new EasyMethod();
+            var expected = 10;
+
+            var actual = Sut.Method2();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
